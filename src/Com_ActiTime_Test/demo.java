35e214791eb6c982a1d8f8851de6d6128de27b7b
CBT <CBT@CBT-2F-301>
2020-12-01 16:15:34 +0530

    version_03

--- src/Com_ActiTime_Test/demo.java
+++ src/Com_ActiTime_Test/demo.java
@@ -1,5 +1,13 @@
 package Com_ActiTime_Test;
 
-public class demo {
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
+public class demo 
+{
+	@Test
+	public void testa()
+	{
+		Reporter.log("Git hub demo...",true);
+	}
 }
